cfc7e896638b49b9e65f2a91efa971a6af610af9
Jared Hanson <jaredhanson@gmail.com>
2013-06-16 19:44:13 -0700

    Process cb messages.

--- main.js
+++ main.js
@@ -1,9 +1,12 @@
 define(['exports',
         'querystring',
+        './lib/guid',
         './lib/transports'],
-function(exports, qs, transports) {
+function(exports, qs, guid, transports) {
 
-  var actions = {};
+  var actions = {}
+    , callbacks = {}
+    , forever = {};
 
   function create(config, cb) {
     cb = cb || function(){};
@@ -21,6 +24,19 @@ function(exports, qs, transports) {
 
   function action(name, handler) {
     actions[name] = handler;
+  }
+  
+  function callback(id, handler, persistent) {
+    if (typeof id == 'function') {
+      handler = id;
+      id = guid();
+    }
+    
+    if (persistent) {
+      forever[id] = true;
+    }
+    callbacks[id] = handler;
+    return id;
   }
   
   function onMessage(message, origin) {
@@ -47,11 +63,22 @@ function(exports, qs, transports) {
       if (!origin) { return; }
       
       var handler = actions[message.xd_action];
-      handler && handler(message);
+      handler && handler();
+      return;
+    }
+    
+    if (message.cb) {
+      var cb = callbacks[message.cb];
+      if (!forever[message.cb]) {
+        delete callbacks[message.cb];
+      }
+      cb && cb(message);
+      return;
     }
   };
   
   exports.create = create;
   exports.action = action;
+  exports.callback = callback;
   
 });
